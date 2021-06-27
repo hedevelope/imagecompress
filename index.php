e5e9b3f3c6bd7546f9b8a59b5d9275412e208382
hedevelope <46059951+hedevelope@users.noreply.github.com>
2021-06-27 22:54:09 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Counselor - Free Bootstrap 4 Template by Colorlib</title>
+  <head >
+    <title>ImageCompress</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
@@ -17,36 +17,47 @@
     
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 
 
-    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-    <script type="text/javascript"></script>
-<![CDATA[
-$(function() { $("#upload_file").on("change", function() { var files = !!this.files ? this.files : []; if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support if (/^image/.test( files[0].type)){ // only image file var reader = new FileReader(); // instance of the FileReader reader.readAsDataURL(files[0]); // read the local file reader.onloadend = function(){ // set image data as background of div $("#previewimg").css("background-image", "url("+this.result+")"); } } }); });
-// ]]></script>
-
-  
  
 
+  
 
   </head>
-  <body>
+  <body> 
+  <style>
+  .contact_section_3{
+	width: 100%;
+	float: left;
+	background-color: #2c2c2c;
+	padding-bottom: 30px;	
+	padding-top: 30px;
+  margin-top: -80px;
+  
+}margin-bottom:100px;
+
+  .contact_taital{
+	width: 100%;
+	float: left;
+	padding-left: 100px;
+	padding-right: 100px;
+	margin-top: 10px;
+}
+.web{
+	background-color: #ffffff;
+	border-radius: 30px;
+	height: auto;
+	padding-top: 20px;	
+	padding-bottom: 20px;
+}
+.londan_text{ 
+  padding-left: 20px;
+  color: black;
+}
 
 
-
-  	<div class="wrap">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 d-flex align-items-center">
-						<p class="mb-0 phone pl-md-2">
-							<a href="#"><span class="fa fa-paper-plane mr-1"></span> flex.compress@gmail.com</a>
-						</p>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-    
+  </style>  
 
     
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
@@ -54,7 +65,7 @@ $(function() { $("#upload_file").on("change", function() { var files = !!this.fi
 	      <a class="navbar-brand" href="index.php">FLEX</a>
 
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
+	        <span class="oi oi-menu"></span> Menü
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
@@ -67,55 +78,9 @@ $(function() { $("#upload_file").on("change", function() { var files = !!this.fi
 	      </div>
 	    </div>
 	  </nav>
-    <!-- END nav -->
+  
     
  
-    <?php
-if(isset($_POST['upload'])){
- 
-  // Dosya adını alalım
-  $filename = $_FILES['imagefile']['name'];
- 
-  // Gelen dosya bir görsel mi?
-  $valid_ext = array('png','jpeg','jpg');
- 
-  // Nereye kaydedelim?
-  $location = "uploads/".$filename;
- 
-  // dosya uzantısı işlemleri
-  $file_extension = pathinfo($location, PATHINFO_EXTENSION);
-  $file_extension = strtolower($file_extension);
- 
-  // uzantı kontrolü
-  if(in_array($file_extension,$valid_ext)){
- 
-    // Resmi sıkıştıralım. Kalitesi 60 olsun. 
-    compressImage($_FILES['imagefile']['tmp_name'],$location,50);
- 
-  }else{
-    echo "Bilinmeyen dosya uzantısı.";
-  }
-}
- 
-// görsel sıkıştırma fonksiyonumuz
-function compressImage($source, $destination, $quality) {
- 
-  $info = getimagesize($source);
- 
-  if ($info['mime'] == 'image/jpeg') 
-    $image = imagecreatefromjpeg($source);
- 
-  elseif ($info['mime'] == 'image/gif') 
-    $image = imagecreatefromgif($source);
- 
-  elseif ($info['mime'] == 'image/png') 
-    $image = imagecreatefrompng($source);
- 
-  imagejpeg($image, $destination, $quality);
- 
-}
- 
-?>
 
 
 
@@ -123,7 +88,7 @@ function compressImage($source, $destination, $quality) {
 
 
 
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_5.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/Custom Size – 1.svg');" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 		  <div class="row no-gutters slider-text align-items-end justify-content-center">
@@ -136,9 +101,8 @@ function compressImage($source, $destination, $quality) {
     	<div class="container">
     		<div class="row justify-content-center pb-5 mb-3">
           <div class="col-md-7 heading-section text-center ftco-animate">
-            <p><font face="tahoma" size="6" style="font-weight:bold">
             <h2>Fotoğrafını Saniyeler İçinde Sıkıştır</h2>
-                        <p><font face="tahoma" size="4" >
+                        
                         <h3>Görüntünü Aşağıdaki Dosya Türlerine Göre Seç</h3>
           </div>
         </div>
@@ -176,15 +140,22 @@ function compressImage($source, $destination, $quality) {
 	        <div class="col-md-4 ftco-animate d-flex">
 	          <div class="block-7 w-100">
 	            <div class="text-center">
-		            <img src="images/gif.PNG" style="width:200px; height: 290px;">
+		            <img src="images/gif.PNG" style="width:210px; height: 290px;">
 		            <ul class="pricing-text mb-5">
 		              
 		            </ul>
 
                 <form method='post' action='' enctype='multipart/form-data'>
-  <input type='file' name='imagefile' >
+  <input type='file' name='imagefile' > 
   <button class="btn btn-primary" type="submit" name="upload" value="Yükle">Submit</button>
 </form>
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
      </div>
 	          </div>
 	        </div>
@@ -192,9 +163,56 @@ function compressImage($source, $destination, $quality) {
     	</div>
     </section>
 
+    <?php
+if(isset($_POST['upload'])){
+ 
+  // Dosya adını alalım
+  $filename = $_FILES['imagefile']['name'];
+ 
+  // Gelen dosya bir görsel mi?
+  $valid_ext = array('png','jpeg','jpg');
+ 
+  // Nereye kaydedelim?
+  $location = "uploads/".$filename;
+ 
+  // dosya uzantısı işlemleri
+  $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+  $file_extension = strtolower($file_extension);
+ 
+  // uzantı kontrolü
+  if(in_array($file_extension,$valid_ext)){
+ 
+    // Resmi sıkıştıralım. Kalitesi 60 olsun. 
+    compressImage($_FILES['imagefile']['tmp_name'],$location,60);
+ 
+  }else{
+    echo "Bilinmeyen dosya uzantısı.";
+  }
+}
+ 
+// görsel sıkıştırma fonksiyonumuz
+function compressImage($source, $destination, $quality) {
+ 
+  $info = getimagesize($source);
+ 
+  if ($info['mime'] == 'image/jpeg') 
+    $image = imagecreatefromjpeg($source);
+ 
+  elseif ($info['mime'] == 'image/gif') 
+    $image = imagecreatefromgif($source);
+ 
+  elseif ($info['mime'] == 'image/png') 
+    $image = imagecreatefrompng($source);
+ 
+  imagejpeg($image, $destination, $quality);
+ 
+}
+ 
+?>
 
 		<section class="ftco-section">
-    	<div class="container">
+    <img src="images/about-top-bg.png">	
+    <div class="container">
     		<div class="row justify-content-center pb-5">
           <div class="col-md-7 heading-section text-center ftco-animate">
             <h2>Kullanım Aşamaları</h2>
@@ -239,11 +257,13 @@ function compressImage($source, $destination, $quality) {
     			</div>
     		</div>
     	</div>
+      <img src="images/about-bottom-bg.png">	
     </section>
 	
 
 
   <section class="ftco-section">
+  <img src="images/about-top-bg.png">
       <div class="container">
         <div class="row justify-content-center pb-5">
           <div class="col-md-7 heading-section text-center ftco-animate">
@@ -289,44 +309,38 @@ function compressImage($source, $destination, $quality) {
           </div>
         </div>
       </div>
-    </section>
-  
-
-
-
-    <section class="ftco-counter ftco-section ftco-no-pt ftco-no-pb img bg-light" id="section-counter">
-    	<div class="container">
-    		<div class="row">
-          <div class="col-md-8 col-lg-3 justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 py-4 mb-4">
-              <div class="text align-items-center">
-                <strong class="number" data-number="3">0</strong>
-                <span>Tamamlanmış Projeler</span>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-7 col-lg-3 justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 py-4 mb-4">
-              <div class="text align-items-center">
-                <strong class="number" data-number="200">0</strong>
-                <span>Memnun Müşteriler</span>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-7 col-lg-3 justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 py-4 mb-4">
-              <div class="text align-items-center">
-                <strong class="number" data-number="350">0</strong>
-                <span>Beğeniler &amp; Yorumlar</span>
-              </div>
-            </div>
-          </div>
-        </div>
-    	</div>
-    </section>
+      <img src="images/about-bottom-bg.png">
+    
 
     <footer class="ftco-footer">
-      <div class="container">
+     
+    <div class="contact_section_3">
+    	<div class="container">
+    		<div class="contact_taital">
+    			<div class="row web">
+    				<div class="col-sm-12 col-md-12 col-lg-4">
+    					<div class="map_main">
+    						<img src="images/map-icon.png">
+    						<span class="londan_text">İstanbul,Avcılar</span>
+    					</div>
+    				</div>
+    				<div class="col-sm-6 col-md-6 col-lg-4">
+    					<div class="map_main">
+    						<img src="images/phone-icon.png">
+    						<span class="londan_text">+212 123 456</span>
+    					</div>
+    				</div>
+    				<div class="col-sm-6 col-md-6 col-lg-4">
+    					<div class="map_main">
+    						<img src="images/email-icon.png">
+    						<span class="londan_text">flex.compress@gmail.com</span>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    
+    
+    <div class="container " style="margin-top:20px;">
         <div class="row mb-5">
           <div class="col-sm-12 col-md">
             <div class="ftco-footer-widget mb-4">
@@ -360,17 +374,10 @@ function compressImage($source, $destination, $quality) {
           </div>
         </div>
       </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);">
-	  Telif Hakkı &copy;<script>document.write(new Date().getFullYear());</script> Tüm hakları gizlidir.
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
+      <div class="copyright_main">
+    	<div class="container">
+    		<p class="copy_text" style="text-align:center;">© 2021 All Rights Reserved. Batuhan Karamemiş & Nesibe Doğan</p>
+    	</div>
       </div>
     </footer>
     
